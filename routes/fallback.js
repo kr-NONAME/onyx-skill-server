8b18cd829feff17d81aec4550bce81c7c12dfdf3
kr-NONAME <pam3149@naver.com>
2025-05-02 16:19:35 +0900

    Update fallback.js

--- routes/fallback.js
+++ routes/fallback.js
@@ -8,60 +8,23 @@ router.post("/", (req, res) => {
     template: {
       outputs: [
         {
-          basicCard: {
-            title: "죄송합니다. 이해하지 못했습니다.",
-            description: "아래에서 원하시는 안내를 선택해주세요.",
-            buttons: [
-              {
-                label: "영업시간",
-                action: "message",
-                messageText: "영업시간",
-              },
-              {
-                label: "위치안내",
-                action: "message",
-                messageText: "위치안내",
-              },
-              {
-                label: "입장료",
-                action: "message",
-                messageText: "입장료",
-              },
-              {
-                label: "제한사항",
-                action: "message",
-                messageText: "제한사항",
-              },
-              {
-                label: "예약문의",
-                action: "message",
-                messageText: "예약문의",
-              },
-              {
-                label: "안내도",
-                action: "message",
-                messageText: "안내도",
-              },
-              {
-                label: "SNS",
-                action: "message",
-                messageText: "SNS",
-              },
-              {
-                label: "EVENT",
-                action: "message",
-                messageText: "EVENT",
-              },
-              {
-                label: "전화번호",
-                action: "message",
-                messageText: "전화번호",
-              },
-            ]
-          }
-        }
-      ]
-    }
+          simpleText: {
+            text: "죄송합니다. 이해하지 못했습니다.\n\n아래에서 원하시는 안내를 선택해주세요.",
+          },
+        },
+      ],
+      quickReplies: [
+        { label: "영업시간", action: "message", messageText: "영업시간" },
+        { label: "위치안내", action: "message", messageText: "위치안내" },
+        { label: "입장료", action: "message", messageText: "입장료" },
+        { label: "제한사항", action: "message", messageText: "제한사항" },
+        { label: "예약문의", action: "message", messageText: "예약문의" },
+        { label: "안내도", action: "message", messageText: "안내도" },
+        { label: "SNS", action: "message", messageText: "SNS" },
+        { label: "EVENT", action: "message", messageText: "EVENT" },
+        { label: "전화번호", action: "message", messageText: "전화번호" },
+      ],
+    },
   });
 });
 
